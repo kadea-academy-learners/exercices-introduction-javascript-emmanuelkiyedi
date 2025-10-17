aa0b0d707181d167104a8622994fdec711168245
emmanuelkiyedi <243emmanuelkiyedi@gmail.com>
2025-10-17 16:24:39 +0100

    final-commit

--- 03-operator-1/exercice_7/main.js
+++ 03-operator-1/exercice_7/main.js
@@ -5,8 +5,8 @@ let ageMere = agePere - 5;
 let ageGrandPere = ageMere * 2 + ageEnfant / 2;
 let ageOncle = agePere + 10;
 
-console.log("age de l'enfant :", ageEnfant);
-console.log("age du père :", agePere);
-console.log("age de la mère :", ageMere);
-console.log("age du grand père :", ageGrandPere);
-console.log("age de l'oncle :", ageOncle);
+console.log("age de l'enfant : " + ageEnfant);
+console.log("age du père : "+ agePere);
+console.log("age de la mère : " + ageMere);
+console.log("age du grand père : " + ageGrandPere);
+console.log("age de l'oncle : " + ageOncle);
